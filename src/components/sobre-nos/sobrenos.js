11df98017f3338ab4b1ae7049fb59e5478b9205c
daniprimo <daniellopes0208@hotmail.com>
2024-04-07 14:29:56 -0300

    Acertando a responsividade do sobre nos

--- src/components/sobre-nos/sobrenos.js
+++ src/components/sobre-nos/sobrenos.js
@@ -1,30 +1,32 @@
-import { Stack } from "@mui/material";
+import './sobrenos.css';
+import { Container, Stack } from "@mui/material";
 import foto from '../../arquivos/fotoFamilia/kirk-removebg-preview.png'
+
 
 export default function SobreNos() {
     return (<>
-        <Stack spacing={2} direction="row" sx={{ marginBottom: 0,
-            backgroundColor: "#000000" }}
-            >
-            <Stack direction="row" style={{paddingRight: '40%',
-                 paddingLeft: '10%',
-                 color: "#ffff"}}>
+    <Container>
+            <Stack spacing={2} direction="row" sx={{ marginBottom: 0,
+                    backgroundColor: "#000000" }}
+                    >
+                    <Stack className='card-do-texto' direction="row" style={{
+                        color: "#ffff"}}>
 
-                    <div style={{
-                        paddingTop: 55
-                    }}>
-                        
-O que é Lorem Ipsum?
+                            <div className="texto-sobrenos">
+                                
+        O que é Lorem Ipsum?
+        <br/>
+        <br/>
+        Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
 
-Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
+                            </div>
 
-                    </div>
+                    </Stack>
+                    <Stack>
+                        <img className="imagem-sobrenos" src={foto}/>
+                    </Stack>
+                </Stack>
 
-
-            <img src={foto} style={{paddingTop: 40}}/>
-
-
-            </Stack>
-        </Stack>
+            </Container>
     </>)
 } 
